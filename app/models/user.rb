c31539766684a5f4da9575e65a78ac9084d5fcee
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-30 16:17:57 +0900

    Userモデルに認証用トークン作成のメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+  before_create :create_ctivation_token
+
+  validates :name,  presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: true
+
+  private
+
+    def create_activation_token
+      self.activation_token = SecureRandom.urlsafe_base64
+      self.activated = false
+    end
 end
