82b4c6c74bd0dd415b94733bf6d2d59eb03164df
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-01 21:37:50 +0900

    UserMailerの設定

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: @user.email, subject: "アカウント承認"
+    mail to: user.email, subject: "アカウント認証"
   end
 end
