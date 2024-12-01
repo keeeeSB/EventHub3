59eb425afbe51d2a6a31b3134beda4f935e823a3
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-01 10:50:30 +0900

    SessionsHelperにメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  def require_login
+    unless logged_in?
+      flash.now[:warning] = "ログインしてください。"
+      redirect_to root_path
+    end
+  end
 end
