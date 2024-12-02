97937e4efc34a4073cf671b6b04809752cb96db8
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-02 11:22:22 +0900

    User, Eventモデルとのリレーションを追加、バリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :start_time, presence: true
+  validates :location, presence: true
 end
