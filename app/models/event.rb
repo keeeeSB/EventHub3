3d251ddf0a46ed9f89c55276fd960642c30ba3db
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-06 09:07:18 +0900

    Eventモデルにスコープを定義

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,19 @@ class Event < ApplicationRecord
   validates :description, presence: true
   validates :start_time, presence: true
   validates :location, presence: true
+
+  # 開催日時が過ぎていないイベント
+  scope :upcoming, -> { where('start_time >= ?', Time.current) }
+  # 人気順に並び替え
+  scope :popular, -> { left_joins(:joins)
+                       .group(:id)
+                       .order('COUNT(joins.id) DESC') }
+
+  # 開催日時が過ぎたイベント
+  scope :past,     -> { where('start_time < ?',  Time.current) }
+  # レビューの平均順に並び替え
+  scope :avarage_reviews_rating, -> { left_joins(:reviews)
+                              .select('events.*, COALESCE(AVG(reviews.rating), 0) AS average_rating')
+                              .group('events.id')
+                              .order(average_rating: :desc)}
 end
