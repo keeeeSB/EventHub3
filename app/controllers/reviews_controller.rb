600f30fac6cc16aec326dea261a5598bdb79d927
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-10 09:32:55 +0900

    レビュー投稿フォームの修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,30 +1,30 @@
 class ReviewsController < ApplicationController
-  before_action :set_event
+  before_action :set_event, only: [:create, :destroy]
 
   def create
-    @review = current_user.reviews.build(event_id: params[:event_id], review_params)
+    @review = current_user.build_review(review_params)
     if @review.save
       flash[:success] = "レビューを作成しました。"
     else
       flash.now[:danger] = "レビューを作成できませんでした。"
     end
-    redirect_to user_event_path(event.user, event)
+    redirect_to user_event_path(@event.user, @event)
   end
 
   def destroy
-    @review = current_user.reviews.find_by(event_id: params[:event_id])
+    @review = Review.find_by(user: current_user, event: @event)
     @review.destroy!
     flash[:success] = "レビューを削除しました。"
-    redirect_to user_event_path(event.user, event), status: :see_other
+    redirect_to user_event_path(@event.user, @event), status: :see_other
   end
 
   private
 
     def review_params
-      params.require(:review).permit(:body, :rating)
+      params.require(:review).permit(:body, :rating).merge(event_id: params[:event_id])
     end
 
     def set_event
-      event = Event.find(params[:event_id])
+      @event = Event.find(params[:event_id])
     end
 end
